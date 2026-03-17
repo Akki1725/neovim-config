6ec117bb270808bd4cbf2185aeaffc24bfdf0275
Akshit Kumar <akshit1725@gmail.com>
2026-03-17 15:56:51 +0530

    Added new formatting convention

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,16 +1,21 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-
     c = { "clang-format" },
     cpp = { "clang-format" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+  },
+
+  formatters = {
+    ["clang-format"] = {
+      prepend_args = {
+        "-style=file:" .. os.getenv("HOME") .. "/.clang-format",
+      },
+    },
   },
 
   format_on_save = {
     timeout_ms = 500,
-    lsp_fallback = true,
+    lsp_fallback = false,
   },
 }
 
--- lua/options.lua
+++ lua/options.lua
@@ -4,6 +4,10 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 vim.opt.shortmess:append("I")
 
